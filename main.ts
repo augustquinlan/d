269716fc2542a3722a8a246f11d2f64778ea1733
August Matthew Quinlan <yoshicat336@gmail.com>
2021-01-11 07:28:50 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("May I play switch?")
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("I am scared.")
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showString("I’m hungry.")
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("I am sad")
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showString("May I go around the block?")
+    basic.clearScreen()
 })
